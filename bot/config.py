10733986ffbc7809db69ec75aefdb5efeaf88d9f
Egor Tyumentsev <egavural@mail.ru>
2025-07-10 03:46:34 +0500

    123

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,13 @@
 import os
 from dataclasses import dataclass
+from dotenv import load_dotenv
+
+load_dotenv()
 
 @dataclass
 class Settings:
-    token: str = os.getenv('BOT_TOKEN', '7783237838:AAFn3X9MqTt-SvJiSA-6KdxJ82ppYyJgoao')
-    admin_id: int = int(os.getenv('ADMIN_ID', '936372207'))
+    token: str = os.getenv('BOT_TOKEN')
+    admin_id: int = int(os.getenv('ADMIN_ID', '0'))
     db_path: str = os.getenv('DB_PATH', 'bot/db/bot.db')
 
 settings = Settings()
