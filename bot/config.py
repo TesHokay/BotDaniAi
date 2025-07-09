9dcc0735bf65e29475ed59eb66d469fdb922b74c
Egor Tyumentsev <egavural@mail.ru>
2025-07-09 22:59:06 +0500

    Замена конфига

--- bot/config.py
+++ bot/config.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Settings:
-    token: str = os.getenv('BOT_TOKEN', 'YOUR_TOKEN_HERE')
-    admin_id: int = int(os.getenv('ADMIN_ID', '0'))
+    token: str = os.getenv('BOT_TOKEN', '7783237838:AAFn3X9MqTt-SvJiSA-6KdxJ82ppYyJgoao')
+    admin_id: int = int(os.getenv('ADMIN_ID', '936372207'))
     db_path: str = os.getenv('DB_PATH', 'bot/db/bot.db')
 
 settings = Settings()
